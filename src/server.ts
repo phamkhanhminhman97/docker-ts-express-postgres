8e5a97b0fb53c3aa8aa24b6a2bc781e8b82ad135
pkmm97 <devtify@Apple.local>
2023-12-29 00:37:54 +0700

    add new package compression

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import { connectToPostgreSQL } from "./db/postgres";
 import dotenv from "dotenv";
+// import compression from "compression";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "dev"}` });
 
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 connectToPostgreSQL();
+// app.use(compression());
 
 app.get("/", async (req, res) => {
   console.log('Enviroment:', process.env.ENVIROMENT);
@@ -17,7 +19,8 @@ app.get("/", async (req, res) => {
       "https://jsonplaceholder.typicode.com/todos/3"
     );
     const todo = response.data;
-    res.json({ message: "Hello, TypeScript and Axios!", todo });
+    const message = "Hello, TypeScript and Axios!";
+    res.json({ message: message, todo });
   } catch (error) {
     console.error("Error fetching data:");
     res.status(500).json({ error: "Internal Server Error" });
